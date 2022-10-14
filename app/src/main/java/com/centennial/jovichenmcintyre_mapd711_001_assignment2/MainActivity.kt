0aaadc491906b0977b326ec30706aa357ae3689c
jovi848 <jovi848.mcintyre@gmail.com>
2022-10-14 15:14:02 -0500

    inital commit

---
+++ app/src/main/java/com/centennial/jovichenmcintyre_mapd711_001_assignment2/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.centennial.jovichenmcintyre_mapd711_001_assignment2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
