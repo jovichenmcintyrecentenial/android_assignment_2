c59935255d949987e32bf192fd891ae466b37bb3
jovi848 <jovi848.mcintyre@gmail.com>
2022-10-14 19:55:30 -0500

    add enumerator and start intent for list of phone activites

--- app/src/main/java/com/centennial/jovichenmcintyre_mapd711_001_assignment2/MainActivity.kt
+++ app/src/main/java/com/centennial/jovichenmcintyre_mapd711_001_assignment2/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.centennial.jovichenmcintyre_mapd711_001_assignment2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import com.centennial.jovichenmcintyre_mapd711_001_assignment2.enumerators.PhoneCompany
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +22,20 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
+        var newIntent = Intent(this, ListOfPhonesActivity::class.java)
+        var selectedCompany:PhoneCompany? = null
+
         when(item.itemId) {
-            R.id.iphone -> Toast.makeText(this,"iphone was clicked",Toast.LENGTH_LONG).show()
-            R.id.samsung -> Toast.makeText(this,"samsung was clicked",Toast.LENGTH_LONG).show()
-            R.id.oppo -> Toast.makeText(this,"samsung was clicked",Toast.LENGTH_LONG).show()
-            R.id.googlepixel -> Toast.makeText(this,"samsung was clicked",Toast.LENGTH_LONG).show()
+            R.id.apple -> selectedCompany = PhoneCompany.APPLE
+            R.id.samsung -> selectedCompany = PhoneCompany.SAMSUNG
+            R.id.oppo -> selectedCompany = PhoneCompany.OPPO
+            R.id.google -> selectedCompany = PhoneCompany.GOOGLE
+        }
+
+        if (selectedCompany != null) {
+            newIntent.putExtra("company", Gson().toJson(selectedCompany))
+            startActivity(newIntent)
+
         }
 
         return super.onOptionsItemSelected(item)
---
+++ app/src/main/java/com/centennial/jovichenmcintyre_mapd711_001_assignment2/enumerators/PhoneCompany.kt
@@ -0,0 +1,20 @@
+package com.centennial.jovichenmcintyre_mapd711_001_assignment2.enumerators
+
+import com.google.gson.annotations.SerializedName
+
+
+enum class PhoneCompany {
+
+    @SerializedName("apple")
+    APPLE,
+
+    @SerializedName("google")
+    GOOGLE,
+
+    @SerializedName("oppo")
+    OPPO,
+
+    @SerializedName("samsung")
+    SAMSUNG,
+
+}
