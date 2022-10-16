44c78ca85b098cd7b2355c3f4d0b6eecfa097641
jovi848 <jovi848.mcintyre@gmail.com>
2022-10-16 16:53:53 -0500

    ui update on checkout page

--- app/src/main/java/com/centennial/jovichenmcintyre_mapd711_001_assignment2/CheckOutActivity.kt
+++ app/src/main/java/com/centennial/jovichenmcintyre_mapd711_001_assignment2/CheckOutActivity.kt
@@ -2,10 +2,16 @@ package com.centennial.jovichenmcintyre_mapd711_001_assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class CheckOutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_out)
+
+        var ccNumber = findViewById<EditText>(R.id.cc_number)
     }
+
+    fun onSubmit(view: View) {}
 }
