0392af68582920f3b53a00c0cc1ade7783d89f36
jovi848 <jovi848.mcintyre@gmail.com>
2022-10-16 14:58:51 -0500

    completed implementation of select color and internal storage size

--- app/src/main/java/com/centennial/jovichenmcintyre_mapd711_001_assignment2/PhoneOptionsSelectActivity.kt
+++ app/src/main/java/com/centennial/jovichenmcintyre_mapd711_001_assignment2/PhoneOptionsSelectActivity.kt
@@ -1,19 +1,24 @@
 package com.centennial.jovichenmcintyre_mapd711_001_assignment2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 import com.centennial.jovichenmcintyre_mapd711_001_assignment2.models.Phone
 import com.google.gson.Gson
 
 class PhoneOptionsSelectActivity : AppCompatActivity() {
+
+    lateinit var selectInternalStorage:String
+    lateinit var  selectColorPhone:String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone_options_select)
+
+        selectInternalStorage = resources.getString(R.string._64_gb)
+        selectColorPhone = resources.getStringArray(R.array.string_of_phone_colors)[0]
 
         var selectedPhone = Gson().fromJson(intent.getStringExtra("phone"),Phone::class.java)
         var phoneImageView = findViewById<ImageView>(R.id.phone_image)
@@ -32,6 +37,18 @@ class PhoneOptionsSelectActivity : AppCompatActivity() {
             spinner.adapter = adapter
         }
 
+        spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                selectColorPhone = resources.getStringArray(R.array.string_of_phone_colors)[position]
+            }
+
+        }
+
 
         phoneNameTextView.text = selectedPhone.name
         val resourceImage: Int = resources.getIdentifier(selectedPhone.uri, "drawable", packageName)
@@ -39,6 +56,19 @@ class PhoneOptionsSelectActivity : AppCompatActivity() {
 
     }
 
-    fun onSelectRadioButton(view: View) {}
-    fun onSubmit(view: View) {}
+    fun onSelectRadioButton(view: View) {
+
+        if(view is RadioButton){
+
+            when(view.id){
+                R.id.opt_64GB -> selectInternalStorage = getString(R.string._64_gb)
+                R.id.opt_128GB -> selectInternalStorage = getString(R.string._128_gb)
+                R.id.opt_256GB -> selectInternalStorage = getString(R.string._256_gb)
+            }
+        }
+
+    }
+    fun onSubmit(view: View) {
+//        var newIntent = Intent(this, )
+    }
 }
